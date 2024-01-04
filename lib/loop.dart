ab93fe87d2dcc6275cd89ee980ffbd9b24aaa861
princymiracle <155509508+princymiracle@users.noreply.github.com>
2024-01-04 16:14:35 +0530

    init commit

---
+++ lib/loop.dart
@@ -0,0 +1,23 @@
+main () {
+
+  for(int a = 1; a<=10; a++){
+    print("Hello World");
+  }
+
+
+ //int no = 100;
+
+  // do{
+  //   print("No is $no");
+  // }while(no<50);
+
+
+  int no = 10;
+
+  while (no <= 50) {
+    print("No is $no");
+    no++;
+  }
+
+
+}
